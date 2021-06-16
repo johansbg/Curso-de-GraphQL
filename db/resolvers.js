fe3c7332ce94d13861a26e6492e0936a36dd0460
Johan Burgos <jsburgos@uninorte.edu.co>
2021-06-16 12:19:50 -0500

    Terminado hasta el modulo 7

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,7 +1,65 @@
+const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({path: 'variables.env'});
+
+const crearToken = (usuario,secreta, expiresIn) =>{
+    const { id, email, nombre, apellido } = usuario;
+    return jwt.sign( { id, email, nombre, apellido }, secreta, { expiresIn } )
+}
+
 //Resolvers
 const resolvers = {
     Query: {
-        obtenerCurso: () => "Algo"
+        obtenerUsuario: async(_, { token }) => {
+            const usuarioId = await jwt.verify(token,process.env.SECRETA)
+            return usuarioId
+        }
+    },
+
+    Mutation: {
+        nuevoUsuario: async (_, { input } ) => {
+            
+            const { email, password } = input
+
+            //Revisar si el usuario ya esta registrado
+            const existeUsuario = await Usuario.findOne({email});
+            if (existeUsuario) {
+                throw new Error('El usuario ya esta registrado')
+            }
+            //Hashear su password
+            const salt = bcryptjs.genSaltSync(10);
+            input.password = bcryptjs.hashSync(password, salt);
+            //Guardar en la DB
+            try {
+                const usuario = new Usuario(input);
+                usuario.save(); //guardarlo
+                return usuario;
+            }catch (error) {
+                console.log(error)
+            }
+        },
+        autenticarUsuario: async (_, { input } ) => {
+
+            const { email, password } = input;
+
+            //Si el usuario existe
+            const existeUsuario = await Usuario.findOne({email});
+            if (!existeUsuario) {
+                throw new Error('El usuario no existe')
+            }
+
+            //Revisar si el password es correcto
+            const passwordCorrecto = await bcryptjs.compare( password, existeUsuario.password);
+            if (!passwordCorrecto){
+                throw new Error('La constraseña es incorrecta')
+            }
+            //Crear Token
+            return{
+                token: crearToken(existeUsuario, process.env.SECRETA,'24h')
+            }
+
+        }
     }
 }
 
--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const {ApolloServer} = require('apollo-server');
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
 
+const conectarDB = require('./config/db');
+
+//Conectar a DB
+conectarDB();
+
 //server
 const server = new ApolloServer({
     typeDefs,
