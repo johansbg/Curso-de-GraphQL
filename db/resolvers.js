eca0e1bcbb393cc08e1c6dfeb0affef0fe3294c8
Johan Burgos <jsburgos@uninorte.edu.co>
2021-06-19 10:38:03 -0500

    backend Terminado

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,6 +1,7 @@
 const Usuario = require('../models/Usuario');
 const Producto = require('../models/Producto');
 const Cliente = require('../models/Cliente');
+const Pedido = require('../models/Pedido');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config({path: 'variables.env'});
@@ -43,6 +44,7 @@ const resolvers = {
 
             return producto
         },
+
         //Querys Clientes
         obtenerClientes: async () => {
             try {
@@ -72,11 +74,105 @@ const resolvers = {
             }
             return cliente
         },
+
+        //Querys Pedidos
+        obtenerPedidos: async () => {
+            try {
+                const pedidos = await Pedido.find({})
+                return pedidos;
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        obtenerPedidosVendedor: async (_, {}, ctx) => {
+            try {
+                const pedidos = await Pedido.find({ vendedor: ctx.usuario.id })
+                return pedidos;
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        obtenerPedido: async (_, { id }, ctx) => {
+            //Si el pedido existe o no
+            const pedido = await Pedido.findById(id);
+            if (!pedido){
+                throw new Error('Pedido no encontrado')
+            }
+            //Solo quien lo creo puede verlo
+            if (pedido.vendedor.toString() !== ctx.usuario.id){
+                throw new Error('No tienes las credenciales')
+            }
+            //retornar resultado
+            return pedido
+        },
+        obtenerPedidosEstado: async (_, { estado }, ctx) => {
+            try {
+                const pedidos = await Pedido.find({ vendedor: ctx.usuario.id, estado });
+                return pedidos;
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        mejoresClientes: async () => {
+            const clientes = await Pedido.aggregate([
+                { $match : { estado : "COMPLETADO" } }, //where
+                { $group : {
+                    _id : "$cliente", 
+                    total: { $sum: '$total' }
+                }}, //group by id cliente
+                {
+                    $lookup: {
+                        from: 'clientes', 
+                        localField: '_id',
+                        foreignField: "_id",
+                        as: "cliente"
+                    } // populate
+                }, 
+                {
+                    $limit: 10
+                }, //Limite
+                {
+                    $sort : { total : -1 }
+                }   //Orden de mayor a menor segun el total
+            ]);
+
+            return clientes;
+        }, 
+
+        mejoresVendedores: async () => {
+            const vendedores = await Pedido.aggregate([
+                { $match : { estado : "COMPLETADO"} },
+                { $group : {
+                    _id : "$vendedor", 
+                    total: {$sum: '$total'}
+                }},
+                {
+                    $lookup: {
+                        from: 'usuarios', 
+                        localField: '_id',
+                        foreignField: '_id',
+                        as: 'vendedor'
+                    }
+                }, 
+                {
+                    $limit: 3
+                }, 
+                {
+                    $sort: { total : -1 }
+                }
+            ]);
+
+            return vendedores;
+        },
+        buscarProducto: async(_, { texto }) => {
+            const productos = await Producto.find({ $text: { $search: texto  } }).limit(10)
+
+            return productos;
+        }
     },
 
     Mutation: {
         //Mutation Usuarios
-
         nuevoUsuario: async (_, { input } ) => {
             
             const { email, password } = input
@@ -225,6 +321,118 @@ const resolvers = {
                 console.log(error);
             }
         },
+
+        //Mutation Pedidos
+        nuevoPedido: async (_, { input }, ctx ) => {
+            //Verificar si el cliente existe o no
+            const { cliente } = input
+
+            let clienteExiste = await Cliente.findById(cliente);
+
+            if (!clienteExiste){
+                throw new Error('Cliente no existe')
+            }
+
+            //Verificar si el cliente es del vendedor
+
+            if (clienteExiste.vendedor.toString() !== ctx.usuario.id){
+                throw new Error('No tienes las credenciales')
+            }
+
+            //Revisar que el stock este disponible
+
+            for await ( const articulo of input.pedido ) {
+                const { id } = articulo;
+                
+                const producto = await Producto.findById(id);
+
+                if (articulo.cantidad > producto.existencia) {
+                    throw new Error(`El articulo: ${producto.nombre} excede la cantidad disponible`);
+                } else {
+                    // restar la cantidad a lo disponible
+                    producto.existencia = producto.existencia - articulo.cantidad;
+                    try {
+                        await producto.save()
+                    } catch (error) {
+                        console.log(error)
+                    }
+                }
+            }
+
+            // Crear un nuevo pedido
+            const nuevoPedido = new Pedido(input);
+            //Asignarle un vendedor
+            nuevoPedido.vendedor = ctx.usuario.id;
+            //Guardarlo en la BD
+            try {
+                const resultado = await nuevoPedido.save();
+                return resultado
+            } catch (error) {
+                console.log(error)                
+            }
+        },
+        actualizarPedido: async(_, {id, input}, ctx) => {
+
+            const { cliente } = input;
+
+            // Si el pedido existe
+            const existePedido = await Pedido.findById(id);
+            if(!existePedido) {
+                throw new Error('El pedido no existe');
+            }
+
+            // Si el cliente existe
+            const existeCliente = await Cliente.findById(cliente);
+            if(!existeCliente) {
+                throw new Error('El Cliente no existe');
+            }
+
+            // Si el cliente y pedido pertenece al vendedor
+            if(existeCliente.vendedor.toString() !== ctx.usuario.id ) {
+                throw new Error('No tienes las credenciales');
+            }
+
+            // Revisar el stock
+            if( input.pedido ) {
+                for await ( const articulo of input.pedido ) {
+                    const { id } = articulo;
+    
+                    const producto = await Producto.findById(id);
+    
+                    if(articulo.cantidad > producto.existencia) {
+                        throw new Error(`El articulo: ${producto.nombre} excede la cantidad disponible`);
+                    } else {
+                        // Restar la cantidad a lo disponible
+                        producto.existencia = producto.existencia - articulo.cantidad;
+    
+                        await producto.save();
+                    }
+                }
+            }
+
+
+
+            // Guardar el pedido
+            const resultado = await Pedido.findOneAndUpdate({_id: id}, input, { new: true });
+            return resultado;
+
+        },
+        eliminarPedido: async (_, {id}, ctx) => {
+            // Verificar si el pedido existe o no
+            const pedido = await Pedido.findById(id);
+            if(!pedido) {
+                throw new Error('El pedido no existe')
+            }
+
+            // verificar si el vendedor es quien lo borra
+            if(pedido.vendedor.toString() !== ctx.usuario.id ) {
+                throw new Error('No tienes las credenciales')
+            }
+
+            // eliminar de la base de datos
+            await Pedido.findOneAndDelete({_id: id});
+            return "Pedido Eliminado"
+        }
     }
 }
 
--- db/schema.js
+++ db/schema.js
@@ -16,6 +16,11 @@ const typeDefs = gql`
         token: String
     }
 
+    type TopVendedor {
+        total: Float
+        vendedor: [Usuario]
+    }
+
     # Productos Types
 
     type Producto {
@@ -36,6 +41,27 @@ const typeDefs = gql`
         telefono: String
         vendedor: ID
     }
+
+    type TopCliente {
+        total: Float
+        cliente: [Cliente]
+    }
+
+    # Pedido Type
+    type Pedido {
+        id: ID
+        pedido: [PedidoGrupo]
+        total: Float
+        cliente: ID
+        vendedor: ID
+        estado: EstadoPedido
+    }
+
+    type PedidoGrupo {
+        id: ID
+        cantidad: Int
+    }
+
     # Usuarios Input
 
     input UsuarioInput {
@@ -66,16 +92,47 @@ const typeDefs = gql`
         telefono: String
     }
 
+    # Pedidos Input
+    input PedidoProductoInput{
+        id: ID
+        cantidad: Int
+        nombre: String
+        precio: Float
+    }
+
+    input PedidoInput {
+        pedido: [PedidoProductoInput]
+        total: Float
+        cliente: ID
+        estado: EstadoPedido
+    }
+
+    # Enum Pedidos
+    enum EstadoPedido {
+        PENDIENTE
+        COMPLETADO
+        CANCELADO
+    }
+    
     type Query {
         # Usuarios
         obtenerUsuario(token: String!): Usuario
-        #Productos
+        # Productos
         obtenerProductos: [Producto]
         obtenerProducto(id: ID!): Producto
-        #Clientes
+        # Clientes
         obtenerClientes: [Cliente]
         obtenerClientesVendedor: [Cliente]
         obtenerCliente(id: ID!): Cliente
+        # Pedidos
+        obtenerPedidos: [Pedido]
+        obtenerPedidosVendedor: [Pedido]
+        obtenerPedido(id: ID!): Pedido
+        obtenerPedidosEstado(estado: String!): [Pedido]
+        # Busquedas Avanzadas
+        mejoresClientes: [TopCliente]
+        mejoresVendedores: [TopVendedor]
+        buscarProducto(texto: String!) : [Producto]
     }
 
     type Mutation {
@@ -88,8 +145,12 @@ const typeDefs = gql`
         eliminarProducto(id: ID!): String
         # Clientes
         nuevoCliente(input: ClienteInput): Cliente
-        actualizarCliente(id: ID!, input:ClienteInput): Cliente
+        actualizarCliente(id: ID!, input: ClienteInput): Cliente
         eliminarCliente(id: ID!): String
+        # Pedidos
+        nuevoPedido(input: PedidoInput): Pedido
+        actualizarPedido(id: ID!, input: PedidoInput ) : Pedido
+        eliminarPedido(id: ID!) : String
     }
 `;
 
